60bd0e6405d1cb95ab0ea9f65a9a5801648ac0b9
Ivan Korekin <c_r_a_c_k@mail.ru>
2022-05-28 01:18:58 +0300

    now matches the JSON description of the object

--- src/main/java/Post.java
+++ src/main/java/Post.java
@@ -1,11 +1,9 @@
 public class Post {
+    public FormDate birthday;
     public String name;
-    public String surname;
+    public String passport;
     public String patronymic;
     public String phone;
-    public String passport;
+    public String surname;
     public boolean subscription;
-
-    public FormDate birthday;
-
 }
